67f9ade76a356d34fefb2589830c57740a28c0e9
KidusMT <adu.kidus@yahoo.com>
2017-11-01 02:07:41 +0300

    made the login and registration

--- app/src/main/java/com/example/kidusmt/multichatapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/kidusmt/multichatapp/activity/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.example.kidusmt.multichatapp;
+package com.example.kidusmt.multichatapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.kidusmt.multichatapp.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/example/kidusmt/multichatapp/activity/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.kidusmt.multichatapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kidusmt.multichatapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
