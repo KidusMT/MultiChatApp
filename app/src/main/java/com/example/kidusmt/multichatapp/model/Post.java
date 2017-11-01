e78f100b5b5108d59fdde51226e1d5ea7d1566ee
KidusMT <adu.kidus@yahoo.com>
2017-11-01 06:19:17 +0300

    last added before morning

---
+++ app/src/main/java/com/example/kidusmt/multichatapp/model/Post.java
@@ -0,0 +1,14 @@
+package com.example.kidusmt.multichatapp.model;
+
+/**
+ * Created by KidusMT on 11/1/2017.
+ */
+
+public class Post {
+
+    public String image_path;
+    public boolean favorite;
+    public String time_posted;
+    public String post_title;
+    public String posted_user;
+}
