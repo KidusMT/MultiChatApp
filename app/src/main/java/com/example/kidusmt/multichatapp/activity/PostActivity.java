83882e43adbaa352e4c8a0af83d9ec791c4b2bd4
KidusMT <adu.kidus@yahoo.com>
2017-11-01 05:50:02 +0300

    added post activity

---
+++ app/src/main/java/com/example/kidusmt/multichatapp/activity/PostActivity.java
@@ -0,0 +1,17 @@
+package com.example.kidusmt.multichatapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.kidusmt.multichatapp.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
---
+++ app/src/main/java/com/example/kidusmt/multichatapp/model/User.java
@@ -0,0 +1,17 @@
+package com.example.kidusmt.multichatapp.model;
+
+/**
+ * Created by Android on 01-11-2017.
+ */
+public class User {
+
+    public String name;
+
+    public String email;
+
+    public String facebookID;
+
+    public String gender;
+
+
+}
